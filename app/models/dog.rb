4357114ae1a3d4cd9557218aded02e65b6e43941
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-02 15:04:14 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed, :age
   @@all = []
   def initialize(name,breed,age)
-    @name, @breed, @age = name, breed, age
-    @@all < self
+    self.name, self.breed, self.age = name, breed, age
+    self.class.all < self
   end
 end
